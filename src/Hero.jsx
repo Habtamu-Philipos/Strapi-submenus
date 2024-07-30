0fc8ae3a86f37e357641bb9bff42390f395cceed
Habtamu-Philipos <habtsh8philip@gmail.com>
2024-07-30 10:11:23 +0300

    final version of Strapi-submenus

---
+++ src/Hero.jsx
@@ -0,0 +1,17 @@
+function Hero() {
+  return (
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>
+          Manage Any Content <br /> Anywhere
+        </h1>
+        <p>
+          Strapi is the leaing open-source heaadless CMS. It's 100% JavaScript
+          and fully customizable
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
